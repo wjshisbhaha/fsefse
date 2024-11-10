b76822fa4b191eff135c7d4b381c7c6331adbad7
wjshisbhaha <110525324+wjshisbhaha@users.noreply.github.com>
2024-11-11 00:28:05 +0800

    Add files via upload

---
+++ MultiScan.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="grid-item">
+    <span class="title">多点扫描</span>
+    <el-row :gutter="10" align="middle">
+      <el-col :span="12">
+        <span>x-start</span>
+        <el-input-number size="small" v-model="xStart" :min="-99999" :max="99999" :step="10"></el-input-number>
+        <el-button @click="adjustValue('xStart', 10)">+10</el-button>
+        <el-button @click="adjustValue('xStart', -10)">-10</el-button>
+      </el-col>
+      <el-col :span="12">
+        <span>x-end</span>
+        <el-input-number size="small" v-model="xEnd" :min="-99999" :max="99999" :step="10"></el-input-number>
+        <el-button @click="adjustValue('xEnd', 10)">+10</el-button>
+        <el-button @click="adjustValue('xEnd', -10)">-10</el-button>
+      </el-col>
+    </el-row>
+    <el-row :gutter="10" align="middle">
+      <el-col :span="12">
+        <span>y-start</span>
+        <el-input-number size="small" v-model="yStart" :min="-99999" :max="99999" :step="10"></el-input-number>
+        <el-button @click="adjustValue('yStart', 10)">+10</el-button>
+        <el-button @click="adjustValue('yStart', -10)">-10</el-button>
+      </el-col>
+      <el-col :span="12">
+        <span>y-end</span>
+        <el-input-number size="small" v-model="yEnd" :min="-99999" :max="99999" :step="10"></el-input-number>
+        <el-button @click="adjustValue('yEnd', 10)">+10</el-button>
+        <el-button @click="adjustValue('yEnd', -10)">-10</el-button>
+      </el-col>
+    </el-row>
+    <div class="button-group">
+      <el-button type="primary" @click="submitMultiScan">提交</el-button>
+    </div>
+    <div v-if="result" class="result">
+      <h3>扫描结果</h3>
+      <pre>{{ result }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      xStart: 0,
+      xEnd: 0,
+      yStart: 0,
+      yEnd: 0,
+      result: null
+    };
+  },
+  methods: {
+    adjustValue(key, amount) {
+      this[key] += amount;
+    },
+    async submitMultiScan() {
+      try {
+        const response = await axios.post('http://127.0.0.1:5000/api/two', {
+          xStart: this.xStart,
+          xEnd: this.xEnd,
+          yStart: this.yStart,
+          yEnd: this.yEnd
+        });
+        this.result = response.data.result; // 将结果保存到 data 中
+        console.log("单点扫描结果：", response.data.result);
+      } catch (error) {
+        console.error("请求出错:", error);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.grid-item {
+  background-color: #fff;
+  border: 2px solid pink;
+  padding: 20px;
+}
+
+.result {
+  margin-top: 20px;
+  background-color: #f0f0f0;
+  padding: 10px;
+  border-radius: 5px;
+}
+</style>
---
+++ SingleScan copy.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="grid-item">
+    <span class="title">单点扫描</span>
+    <el-row :gutter="10" type="flex" align="middle">
+      <el-col :span="12">
+        <span>x-start</span>
+        <el-input-number size="small" v-model="xStart" :min="-99999" :max="99999" :step="10"></el-input-number>
+        <el-button @click="adjustValue('xStart', 10)">+10</el-button>
+        <el-button @click="adjustValue('xStart', -10)">-10</el-button>
+      </el-col>
+      <el-col :span="12">
+        <span>x-end</span>
+        <el-input-number size="small" v-model="xEnd" :min="-99999" :max="99999" :step="10"></el-input-number>
+        <el-button @click="adjustValue('xEnd', 10)">+10</el-button>
+        <el-button @click="adjustValue('xEnd', -10)">-10</el-button>
+      </el-col>
+    </el-row>
+    <div class="button-group">
+      <el-button type="primary" @click="submitSingleScan">提交</el-button>
+    </div>
+    <div v-if="result" class="result">
+      <h3>扫描结果</h3>
+      <pre>{{ result }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      xStart: 0,
+      xEnd: 0,
+      result: null
+    };
+  },
+  methods: {
+    adjustValue(key, amount) {
+      this[key] += amount;
+    },
+    async submitSingleScan() {
+      try {
+        const response = await axios.post('http://127.0.0.1:5000/api/process', {
+          xStart: this.xStart,
+          xEnd: this.xEnd
+        });
+        this.result = response.data.result;
+        console.log("单点扫描结果：", response.data.result);
+      } catch (error) {
+        console.error("请求失败：", error);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.grid-item {
+  background-color: #fff;
+  border: 2px solid pink;
+  padding: 20px;
+}
+.title {
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+.button-group {
+  margin-top: 20px;
+  text-align: center;
+}
+.result {
+  margin-top: 20px;
+  background-color: #f0f0f0;
+  padding: 10px;
+  border-radius: 5px;
+}
+</style>
