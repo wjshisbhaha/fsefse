144004d5d8fd0cba2830f0a37761ab548a25adb5
wjshisbhaha <110525324+wjshisbhaha@users.noreply.github.com>
2024-11-11 00:26:25 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 // src/router/index.js
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Login from '../components/Login.vue'; // 确保路径正确
+import Login from '../components/Login.vue'; // 引入主页组件
+import SingleScan from '../components/SingleScan.vue';
+import MultiScan from '../components/MultiScan.vue';
+import DataCenter from '../components/DataCenter.vue';
 
 Vue.use(VueRouter);
 
+
+const originalPush = VueRouter.prototype.push
+
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 const routes = [
   {
-    path: '/login',
-    component: Login
+    path: '/',
+    name: 'Login',
+    component: Login // 将主页组件设置为根路径
   },
   {
-    path: '/',
-    redirect: '/login' // 重定向到 /login
+    path: '/single-scan',
+    name: 'SingleScan',
+    component: SingleScan
+  },
+  {
+    path: '/multi-scan',
+    name: 'MultiScan',
+    component: MultiScan
+  },
+  {
+    path: '/data-center',
+    name: 'DataCenter',
+    component: DataCenter
+  },
+  {
+    path: '*', // 捕获所有未定义路径，重定向到主页
+    redirect: '/'
   }
 ];
 
